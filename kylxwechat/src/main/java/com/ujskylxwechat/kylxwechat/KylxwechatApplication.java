e8d13c7d962dd956a4a9b1fc1171e06e7301b0d3
fireinsect <847954981@qq.com>
2021-10-18 10:27:46 +0800

    添加了spring

---
+++ kylxwechat/src/main/java/com/ujskylxwechat/kylxwechat/KylxwechatApplication.java
@@ -0,0 +1,13 @@
+package com.ujskylxwechat.kylxwechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KylxwechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KylxwechatApplication.class, args);
+	}
+
+}
---
+++ kylxwechat/src/test/java/com/ujskylxwechat/kylxwechat/KylxwechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujskylxwechat.kylxwechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KylxwechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
